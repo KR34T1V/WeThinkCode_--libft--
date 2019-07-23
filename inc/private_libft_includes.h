e79e621c8c7eb00baf50ad60d3062949d623a926
m4dm0nk3y <m4dm0nk3y.za@gmail.com>
2019-07-23 22:29:04 +0200

    headers

--- inc/private_libft_includes.h
+++ inc/private_libft_includes.h
@@ -1,6 +1,11 @@
 #ifndef PRIVATE_LIBFT_INCLUDES
 # define PRIVATE_LIBFT_INCLUDES
 
+# define BUFF_SIZE 500
+# define BOLEAN int
+# define TRUE 1
+# define FALSE 0
+
 # include <inttypes.h>
 # include <dirent.h>
 # include <stdarg.h>
